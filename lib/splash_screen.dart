68e035b5d35e3f3787ef1df3c305a9a1a0d71082
ScribbleRiyad <scribbleriyad@gmail.com>
2023-05-31 12:25:39 +0600

    project Setup

---
+++ lib/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:portfolio_app/presentation/home.dart';
+
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(
+        const Duration(seconds: 8),
+        () => Navigator.push(
+            context, CupertinoPageRoute(builder: (_) => const HomeScreen())));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SizedBox(
+            height: 250.00,
+            child: Image.asset('assets/images/scribble-logo-animation.gif')),
+      ),
+    );
+  }
+}
